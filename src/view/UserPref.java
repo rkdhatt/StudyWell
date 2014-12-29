064b8e72f29e09efd3d4df000b64e042218ee4d1
jwoo12 <jwoo@ualberta.ca>
2014-12-28 20:09:45 -0700

    Changed UserPref to add xml and commented buggy code in CourseDataSource

--- src/view/UserPref.java
+++ src/view/UserPref.java
@@ -1,5 +1,7 @@
 package view;
 
+import com.calc.gpacalculator.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,7 +12,10 @@ public class UserPref extends ActionBarActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
+	    setContentView(R.layout.userpref);
+	    
+	    
+	    
 	    // TODO Auto-generated method stub
 	}
 
